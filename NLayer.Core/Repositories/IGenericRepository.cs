f7df17ecf787d068577d314ba7c9b99fcf39b099
ONUR ROZET <onur.rozet@sabah.com.tr>
2023-10-10 15:13:59 +0300

    Add project files.

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+	public interface IGenericRepository<T> where T : class, new()
+	{
+		Task<T> GetByIdAsync(int id);
+
+		IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+		IQueryable<T> where(Expression<Func<T, bool>> expression);
+
+		Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+		Task AddAsync(T entity);
+
+		Task AddRangeAsync(IEnumerable<T> entities);
+
+		void Update(T entity);
+
+		void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entities);
+	}
+}
